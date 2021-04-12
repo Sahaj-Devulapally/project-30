46fde529ae6878d627ab53683d677e45fee04314
Sahaj-Devulapally <76732777+Sahaj-Devulapally@users.noreply.github.com>
2021-04-12 11:16:18 +0530

    Add files via upload

--- Box.js
+++ Box.js
@@ -11,22 +11,24 @@ class Box{
       this.width = width;
       this.height = height;
       World.add(world, this.body);
-      this.visibility=100
+      this.visibility=255
     }
     display(){
-      var angle = this.body.angle;
-      var pos= this.body.position;
-      push();
-      translate(pos.x, pos.y);
-      rotate(angle);
-      rectMode(CENTER);
-      rect(0,0,this.width, this.height);
-      if(this.body.speed>3){
-        World.remove(world, this.body);
-      this.visibility=this.visibility-5
-      tint(255,this.visibility)
-      rect(this.body.position.x,this.body.position.y,50,50)
-      }
-      pop();
+     if(this.body.speed<3){
+var angle=this.body.angle
+var pos=this.body.position
+push()
+translate(pos.x,pos.y)
+rotate(angle)
+rectMode(CENTER)
+rect(0,0,this.width,this.height)
+pop()
+} else{
+  World.remove(world,this.body)
+  push()
+  this.visibility=this.visibility-5
+  tint(255,this.visibility)
+  pop()
+}
     }
 }
